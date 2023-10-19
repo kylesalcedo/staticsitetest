f96e9587cf7249e7c75725d0c36b538d05de118a
Dr. Kyle Salcedo, PT, DPT <kylesalcedo@users.noreply.github.com>
2023-10-19 09:57:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript for dropdown menu
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("menu");
+
+    menuButton.addEventListener("click", function () {
+        menu.classList.toggle("show-menu");
+    });
+});
